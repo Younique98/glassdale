c0eedd5a2567e26cfc48394735a95e3b172085c4
Erica Thompson <younique98@gmail.com>
2021-01-22 14:11:22 -0800

    fixed files with code along

---
+++ scripts/Officers/OfficerProvider.js
@@ -0,0 +1,20 @@
+let officers = []    
+
+export const useOfficers = () => {
+    return officers.slice()
+}
+
+export const getOfficers = () => {
+    return fetch("https://criminals.glassdale.us/officers")
+        .then(response => response.json())
+        .then(
+            parsedOfficers => {
+                console.table(parsedOfficers)
+                officers = parsedOfficers
+            }
+        )
+}
+
+//  export const gotOfficers = officers
+// console.log(gotOfficers)
+
