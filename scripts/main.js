2fba747a96146f48a4b013640b368978c928666c
Erica Thompson <younique98@gmail.com>
2021-01-26 14:03:02 -0800

    added Event Listener to Criminal list

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,10 @@
 console.log("Hello World")
-import { Officer } from './Officers/officers.js'
-import { useOfficers } from './Officers/OfficerProvider.js'
+
 import { OfficerList } from './Officers/officerList.js'
-
-
-// const allTheOfficers = useOfficers()
-// for (const officer of allTheOfficers) {
-//     console.log(officer)
-// };
-
-
 OfficerList()
 
-import {Criminal} from './criminal/criminals.js'
-import {useCriminals} from './criminal/CriminalProvider.js'
 import {CriminalList} from './criminal/criminalList.js'
-
 CriminalList()
-
-
 
 import { ConvictionSelect } from './convictions/ConvictionSelect.js'
 ConvictionSelect()
-
-// import { convictionsCollection} from './convictions/ConvictionSelect.js'
-// convictionsCollection ()
