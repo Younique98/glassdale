608e0207809da814a87e543114a7ab2d0e48c54e
Erica Thompson <younique98@gmail.com>
2021-01-28 08:05:15 -0800

    removed console.tables and console.logs to clean up console

--- scripts/criminal/criminals.js
+++ scripts/criminal/criminals.js
@@ -4,6 +4,10 @@ export const Criminal = (criminal) => {
     <div class="criminal">
    <p class="criminal__name"> Name: ${criminal.name} </p>
     <p class="criminal__age"> Age: ${criminal.age}    </p>
+    <p class="criminal__arrestingOfficer"> Arresting Officer: ${criminal.arrestingOfficer}    </p>
+    <p class="criminal__conviction"> Conviction: ${criminal.conviction}    </p>
+    
+
     <p class="criminal__start"> Incarceration Start Date: ${new Date(criminal.incarceration.start).toLocaleDateString('en-US')} </p>
    <p class="criminal__end"> Incarceration End Date: ${new Date(criminal.incarceration.end).toLocaleDateString('en-US')} </p>
     </div>
