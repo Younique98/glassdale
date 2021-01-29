0a8038cd82b4490637a42872f11146e74ac4d5c3
Erica Thompson <younique98@gmail.com>
2021-01-29 13:04:59 -0800

    added criminal known assocciate button

--- scripts/criminal/criminals.js
+++ scripts/criminal/criminals.js
@@ -1,4 +1,5 @@
 export const Criminal = (criminal) => {
+    console.log(criminal)
     return `
     <section class="criminalCard">
     <div class="criminal">
@@ -6,11 +7,15 @@ export const Criminal = (criminal) => {
     <p class="criminal__age"> Age: ${criminal.age}    </p>
     <p class="criminal__arrestingOfficer"> Arresting Officer: ${criminal.arrestingOfficer}    </p>
     <p class="criminal__conviction"> Conviction: ${criminal.conviction}    </p>
+    <p class="criminal__knownAssociate"> Known Associate: ${criminal.known_associates[0][0]}    </p>
+    <p class="criminal__alibis"> Alibis Provided: ${criminal.known_associates[0][1]}    </p>
     
 
     <p class="criminal__start"> Incarceration Start Date: ${new Date(criminal.incarceration.start).toLocaleDateString('en-US')} </p>
    <p class="criminal__end"> Incarceration End Date: ${new Date(criminal.incarceration.end).toLocaleDateString('en-US')} </p>
-    </div>
+    
+   </div>
+     <button id="associates--${criminal.id}">Associate Alibis</button>
     </section>
     `
 }
