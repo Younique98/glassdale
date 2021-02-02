0690da413650279a757253a5e5227e2a69f883ec
Erica Thompson <younique98@gmail.com>
2021-02-02 08:56:12 -0800

    fixed renderToDOm function to push html to DOM and added styling to the alibi box

--- scripts/KnownAssociates/KnownAssociateButton.js
+++ scripts/KnownAssociates/KnownAssociateButton.js
@@ -1,17 +1,4 @@
-// document.getElementById("associates--${criminal.id}").addEventListener("click", function() {
-//   document.querySelector(".criminalsAssociate").innerHTML = `
-//   <p class="criminal__knownAssociate"> Known Associate: ${criminal.known_associates[0].name}    </p>
-//     <p class="criminal__alibis"> Alibis Provided: ${criminal.known_associates[0].alibi}    </p>
-//     `
-// });
 
-// export const KnownAssociateAndAlibi = (knowingAssociate) => {
-//   return `
-//   <p class="criminal__knownAssociate"> Known Associate: ${criminal.known_associates[0].name}    </p>
-//     <p class="criminal__alibis"> Alibis Provided: ${criminal.known_associates[0].alibi}    </p>
-    
-//   `
-// }
 
 const contentTarget = document.querySelector(".criminalsAssociate")
 const eventHub = document.querySelector(".container")
@@ -21,7 +8,7 @@ eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id.startsWith("associates--")) {
         
         const [prefix, criminalId] = clickEvent.target.id.split("--")
-        console.log(prefix, clickEvent)
+        // console.log(prefix, clickEvent)
        
         const customEvent = new CustomEvent("showAssociatesClicked", {
             detail: {criminalIdTaco: criminalId}
--- scripts/KnownAssociates/theAssociate.js
+++ scripts/KnownAssociates/theAssociate.js
@@ -1,12 +1,11 @@
 
 export const KnownAssociate = (associate) => {
-     console.log("known associates maybe", associate)
+     
     return `
-    <section class="associateCard">
-    <div class="associate">
-   <p class="associate__knownAssociate"> Known Associate: ${criminal.known_associates[0].name}    </p>
-    <p class="associate__alibis"> Alibis Provided: ${criminal.known_associates[0].alibi}    </p>
-   </div>
-    </section>
+    <div class = "associate">
+    <h3>${associate.known_associates[0].name}</h3>
+    <p>Alibi: ${associate.known_associates[0].alibi}</p>
+    
+    </div>
     `
 }
